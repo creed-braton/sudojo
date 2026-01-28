5036027dac953d1f3aec7c5b4ca47c77ffc1aef7
creed-braton <creed-braton@mailbox.org>
2026-01-28 20:41:04 +0100

    enha: dynamic game layout

--- client/src/components/Game/Game.tsx
+++ client/src/components/Game/Game.tsx
@@ -1,15 +1,10 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  type ReactElement,
-  type RefObject,
-} from "react";
+import { type ReactElement } from "react";
 import Board from "../Board/Board";
 import Input from "../Input/Input";
 import type { Cell, Position } from "../../providers/board";
 import type { InputMode } from "../../providers/input";
 import style from "./Game.module.css";
+import Info from "../Info/Info";
 
 type GameProps = {
   board: Cell[][];
@@ -30,32 +25,15 @@ const Game = ({
   toggleNotes,
   input,
 }: GameProps): ReactElement => {
-  const boardRef: RefObject<HTMLTableElement | null> =
-    useRef<HTMLTableElement>(null);
-  const [boardWidth, setBoardWidth] = useState<number>(0);
-
-  useEffect(() => {
-    if (!boardRef.current) return;
-
-    const observer: ResizeObserver = new ResizeObserver((entries) => {
-      setBoardWidth(entries[0].contentRect.width);
-    });
-
-    observer.observe(boardRef.current);
-    return () => observer.disconnect();
-  }, [board]);
-
   return (
     <div className={style.game}>
-      <div className={style.board}>
-        <Board
-          board={board}
-          cursor={cursor}
-          setCursor={setCursor}
-          ref={boardRef}
-        />
+      <div className={style.info}>
+        <Info />
       </div>
-      <div style={{ maxWidth: `${boardWidth}px` }}>
+      <div className={style.board}>
+        <Board board={board} cursor={cursor} setCursor={setCursor} />
+      </div>
+      <div className={style.input}>
         <Input
           mode={mode}
           togglePing={togglePing}
---
+++ client/src/components/Info/Info.tsx
@@ -0,0 +1,8 @@
+import type { ReactElement } from "react";
+import style from "./Info.module.css";
+
+const Info = (): ReactElement => {
+  return <div className={style.info}></div>;
+};
+
+export default Info;
